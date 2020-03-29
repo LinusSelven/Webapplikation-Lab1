83d2ca7067e705e6e86f1815c0c6c6953b04e4f6
Linus S <skoorps@gmail.com>
2020-03-29 16:47:00 +0200

    small style changse

--- login/js/errors.js
+++ login/js/errors.js
@@ -3,12 +3,9 @@ let password = document.getElementById('password')
 
 
 form.addEventListener('submit', (e) => {
-    console.log(password.value)
-    console.log(password.value.length);
   if(password.value.length < 6) {
       const passError = document.getElementById('passwordError')
       passError.innerHTML = 'Password must contain more than 6 characters'
       e.preventDefault();
   }
-    e.preventDefault();
 });
